91eee7c889b2be69eea1bc41fb0ad9d269f4d4e6
PacketInspector <packetinspector@users.noreply.github.com>
2017-10-10 16:39:19 -0400

    Create config-backup.py

---
+++ Random/config-backup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3.4
+import os
+from pytos.securetrack.helpers import Secure_Track_Helper
+
+#Files go here
+confbackdir = '/tmp/tufin/'
+t_ip = '<IP>'
+t_user = '<USER>'
+t_pass = '<PASS>'
+
+# No edit past here...
+st_helper = Secure_Track_Helper(t_ip, (t_user, t_pass))
+print("Fethching Devices...")
+try:
+    devices = st_helper.get_devices_list()
+except:
+    print("Error reading devices")
+    exit()
+
+if (not os.path.exists(confbackdir)):
+    os.makedirs(confbackdir)
+
+# print(devices)
+for d in devices:
+    filename = d.name + '-' + str(d.id)
+    print("Getting config for {}".format(filename))
+    try:
+        c = st_helper.get_device_config_by_id(d.id)
+    except:
+        print("Config failed for {}".format(filename))
+        continue
+
+    print("Success!. Saving Config")
+    c = bytes.decode(c)
+    c.replace('\n',"\n")
+    with open(confbackdir + filename + '.txt', 'w') as f:
+        print(c,file=f)
